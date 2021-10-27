1f3a9bf98f7728461ed24b1216ed0951e9ca69fc
Iñigo <iurizar@outlook.com>
2021-10-27 19:25:04 +0200

    Primer commit. Subida de proyecto.

---
+++ src/main/java/com/birt/berton/BertonRestAPI_PROY_1C/BertonRestApiProy1CApplication.java
@@ -0,0 +1,13 @@
+package com.birt.berton.BertonRestAPI_PROY_1C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BertonRestApiProy1CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BertonRestApiProy1CApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birt/berton/BertonRestAPI_PROY_1C/BertonRestApiProy1CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birt.berton.BertonRestAPI_PROY_1C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BertonRestApiProy1CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
